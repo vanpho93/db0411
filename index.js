166c86de2db1875902cefbaf902572fa53cff600
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-02 10:42:39 +0700

    crud

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.get('/word', (req, res) => {
     // send tat ca cac tu vung hien co
     Word.find({})
-    .then(words => res.send(words))
+    .then(words => res.send({ isSuccess: true, words }))
     .catch(error => res.status(404).send({
         isSuccess: false,
         message: error.message
@@ -19,18 +19,47 @@ app.get('/word', (req, res) => {
 
 app.get('/word/:id', (req, res) => {
     // send tat ca cac tu vung hien co
+    const { id } = req.params;
+    Word.findById(id)
+    .then(word => res.send({ isSuccess: true, word }))
+    .catch(error => res.status(404).send({
+        isSuccess: false,
+        message: error.message
+    }));
 });
 
 app.post('/word', (req, res) => {
     // Them 1 tu vung
+    const { en, vn } = req.body;
+    const word = new Word({ en, vn });
+    word.save()
+    .then(word => res.send({ isSuccess: true, word }))
+    .catch(error => res.status(404).send({
+        isSuccess: false,
+        message: error.message
+    }));
 });
 
 app.put('/word', (req, res) => {
+    const { en, vn, isMemorized, id } = req.body;
+    Word.findByIdAndUpdate(id, { en, vn, isMemorized })
+    .then(word => res.send({ isSuccess: true, word }))
+    .catch(error => res.status(404).send({
+        isSuccess: false,
+        message: error.message
+    }));
     // Update 1 tu vung
 });
 
 app.delete('/word/:id', (req, res) => {
     // Xoa 1 tu vung cu the
+    const { id } = req.params;
+    Word.findByIdAndRemove(id)
+    .then(word => res.send({ isSuccess: true, word }))
+    .catch(error => res.status(404).send({
+        isSuccess: false,
+        message: error.message
+    }));
 });
 
 app.listen(3000, () => console.log('Server started'));
