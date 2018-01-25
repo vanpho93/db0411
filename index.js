48ae6895d92bebc85e4c0d4cfc0cb58751ac41aa
Van Pho Nguyen <vanpho02@gmail.com>
2018-01-25 19:50:53 +0700

    server words

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-// Doc tat ca tu vung
-
 app.get('/word', (req, res) => {
-    // send tat ca cac tu vung hien co
     Word.find({})
     .then(words => res.send({ isSuccess: true, words }))
     .catch(error => res.status(404).send({
@@ -18,10 +15,12 @@ app.get('/word', (req, res) => {
 });
 
 app.get('/word/:id', (req, res) => {
-    // send tat ca cac tu vung hien co
     const { id } = req.params;
     Word.findById(id)
-    .then(word => res.send({ isSuccess: true, word }))
+    .then(word => {
+        if (!word) throw new Error('Cannot find word.');
+        res.send({ isSuccess: true, word });
+    })
     .catch(error => res.status(404).send({
         isSuccess: false,
         message: error.message
@@ -29,7 +28,6 @@ app.get('/word/:id', (req, res) => {
 });
 
 app.post('/word', (req, res) => {
-    // Them 1 tu vung
     const { en, vn } = req.body;
     const word = new Word({ en, vn });
     word.save()
@@ -40,22 +38,26 @@ app.post('/word', (req, res) => {
     }));
 });
 
-app.put('/word', (req, res) => {
-    const { en, vn, isMemorized, id } = req.body;
-    Word.findByIdAndUpdate(id, { en, vn, isMemorized })
-    .then(word => res.send({ isSuccess: true, word }))
+app.put('/word/:_id', (req, res) => {
+    const { en, vn, isMemorized } = req.body;
+    Word.findByIdAndUpdate(req.params._id, { en, vn, isMemorized })
+    .then(word => {
+        if (!word) throw new Error('Cannot find word.');
+        res.send({ isSuccess: true, word });
+    })
     .catch(error => res.status(404).send({
         isSuccess: false,
         message: error.message
     }));
-    // Update 1 tu vung
 });
 
 app.delete('/word/:id', (req, res) => {
-    // Xoa 1 tu vung cu the
     const { id } = req.params;
     Word.findByIdAndRemove(id)
-    .then(word => res.send({ isSuccess: true, word }))
+    .then(word => {
+        if (!word) throw new Error('Cannot find word.');
+        res.send({ isSuccess: true, word });
+    })
     .catch(error => res.status(404).send({
         isSuccess: false,
         message: error.message
